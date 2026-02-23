96a851d2bbf98c9447f3d435cb5f79dc2830f43b
Micaela Cairoli <mcairoli@altec.com.ar>
2026-02-23 20:21:12 -0300

    probemossss

--- dashboard.php
+++ dashboard.php
@@ -7,5 +7,7 @@ if (!is_writable($path)) @chmod($path, 0777);
 session_save_path($path);
 session_start();
 
-echo "<h1 style='background:#d1ffd1;padding:20px'>TEST 2 OK (solo sesiones)</h1>";
+require __DIR__ . '/includes/auth.php';
+
+echo "<h1 style='background:#d1ffd1;padding:20px'>TEST 3 OK (auth.php)</h1>";
 exit;
