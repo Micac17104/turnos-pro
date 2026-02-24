57430d5f55710a9676c38ab574084367a9c531f4
Micaela Cairoli <mcairoli@altec.com.ar>
2026-02-23 22:29:09 -0300

    no rezaste

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "<pre>DEBUG ACTIVADO</pre>";
+
 // --- SESIONES ---
 $path = __DIR__ . '/sessions';
 if (!is_dir($path)) mkdir($path, 0777, true);
