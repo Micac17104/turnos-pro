0385c99173d80a9bca765663cb8596fddf177608
Micaela Cairoli <mcairoli@altec.com.ar>
2026-03-15 20:12:24 -0300

    cambioo

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+    exit;
+
+    // resto del código...
+}
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
