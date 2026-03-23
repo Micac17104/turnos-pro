58758014c67e3055ea4e669c7858d93652bb1098
Micaela Cairoli <mcairoli@altec.com.ar>
2026-03-23 11:20:14 -0300

    cambios

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit0393830ad13082bb726d7f225d4a3f36
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit0393830ad13082bb726d7f225d4a3f36', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit0393830ad13082bb726d7f225d4a3f36', 'loadClassLoader'));
