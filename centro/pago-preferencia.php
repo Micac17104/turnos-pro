26860f5ba47c26db9469c24ed3b05dae15b5f9de
Micaela Cairoli <mcairoli@altec.com.ar>
2026-03-24 11:40:50 -0300

    prueba una mas

--- centro/pago-preferencia.php
+++ centro/pago-preferencia.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 require __DIR__ . '/../pro/includes/db.php';   // PRIMERO LA DB
-require '/app/vendor/autoload.php';           // DESPUÉS EL AUTOLOAD
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 use MercadoPago\SDK;
 use MercadoPago\Preapproval;
