75bbf384d41bc797863a83afa5caf06d80df34cf
Micaela Cairoli <mcairoli@altec.com.ar>
2026-03-27 00:20:16 -0300

    Add vendor for Railway

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ecc163a5ca5793b3f656f82ba08f5de22bf51fb6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ecc163a5ca5793b3f656f82ba08f5de22bf51fb6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mercadopago/sdk' => array(
+            'pretty_version' => 'v0.5.6',
+            'version' => '0.5.6.0',
+            'reference' => '67f65735d5d720a7fc2569163f5c7d150adc613d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mercadopago/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
