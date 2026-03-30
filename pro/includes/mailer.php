ce6d6d7b8245bf7ab3c6b9d095305ae4f0645459
Micaela Cairoli <mcairoli@altec.com.ar>
2026-03-30 16:43:00 -0300

    cambioss

--- pro/includes/mailer.php
+++ pro/includes/mailer.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// Ajustamos la ruta para cargar PHPMailer desde /auth
+// Cargar PHPMailer desde /auth/PHPMailer/src/
 require __DIR__ . '/../auth/PHPMailer/src/Exception.php';
 require __DIR__ . '/../auth/PHPMailer/src/PHPMailer.php';
 require __DIR__ . '/../auth/PHPMailer/src/SMTP.php';
@@ -15,13 +15,18 @@ function enviarEmail($destinatario, $asunto, $mensajeHtml) {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'TU_EMAIL@gmail.com';
-        $mail->Password = 'TU_APP_PASSWORD';
+
+        // 👉 REEMPLAZAR ESTO POR TU EMAIL
+        $mail->Username = 'turnospro2@gmail.com';
+
+        // 👉 REEMPLAZAR ESTO POR TU APP PASSWORD
+        $mail->Password = 'ybuuunbdkeyeziql';
+
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
-        // Remitente
-        $mail->setFrom('TU_EMAIL@gmail.com', 'TurnosAura');
+        // Remitente (debe ser el mismo email del Username)
+        $mail->setFrom('turnospro2@gmail.com', 'TurnosAura');
 
         // Destinatario
         $mail->addAddress($destinatario);
