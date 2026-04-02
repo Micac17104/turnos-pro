70dae90b5f760109200b504288446062daaf2a5c
Micaela Cairoli <mcairoli@altec.com.ar>
2026-04-02 00:08:34 -0300

    nuevo

--- auth/mailer.php
+++ auth/mailer.php
@@ -20,24 +20,26 @@ function enviarEmail($to, $subject, $body) {
         // SendGrid SIEMPRE usa "apikey" como usuario
         $mail->Username = 'apikey';
 
-        // La API KEY ahora viene de una variable de entorno
+        // La API KEY viene de variable de entorno
         $mail->Password = getenv('SENDGRID_API_KEY');
 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
-        $mail->setFrom('turnospro2@gmail.com', 'TurnosAura');
+        // TU EMAIL REAL
+        $mail->setFrom('turnospro2@gmail.com', 'TurnosPro');
+
         $mail->addAddress($to);
 
         $mail->isHTML(true);
         $mail->Subject = $subject;
-        $mail->Body    = nl2br($body);
+        $mail->Body = $body;
 
-        return $mail->send();
+        $mail->send();
+        return true;
 
     } catch (Exception $e) {
-        error_log("ERROR SMTP: " . $e->getMessage());
+        error_log("Error enviando email: " . $mail->ErrorInfo);
         return false;
     }
 }
-
