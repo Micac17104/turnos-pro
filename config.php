2935d9ebb65b2852e0446f4058ed02fa4747f629
Micaela Cairoli <mcairoli@altec.com.ar>
2026-04-02 14:40:12 -0300

    Agrego config.php correcto

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+putenv("GMAIL_APP_PASSWORD=qwom wmrp ckhz jmqt");
+
+$host = "mysql.railway.internal";
+$dbname = "railway";
+$user = "root";
+$pass = "yjdBrLZlWrojESOUCJNSIySVjkcqAlXf";
+$port = 3306;
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
