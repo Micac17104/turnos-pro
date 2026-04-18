b6a914b08eeadb86b79dfb18d5d8a66873072358
Micaela Cairoli <mcairoli@altec.com.ar>
2026-04-18 16:11:45 -0300

    otro mas

--- pro/dashboard.php
+++ pro/dashboard.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/includes/auth.php';
 require __DIR__ . '/includes/db.php';
 require __DIR__ . '/includes/helpers.php';
-require __DIR__ . '/includes/auth-profesional.php';
+
 
 
 
