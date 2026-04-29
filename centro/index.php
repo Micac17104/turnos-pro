7e13a0de98f67f814f456503ca1778489a597368
Micaela Cairoli <mcairoli@altec.com.ar>
2026-04-29 01:33:12 -0300

    oro

--- centro/index.php
+++ centro/index.php
@@ -1,13 +1,4 @@
 <?php
-echo "INDEX OK<br>";
-
-echo "Buscando archivo: " . __DIR__ . "/centro-landing.php<br>";
-
-if (file_exists(__DIR__ . "/centro-landing.php")) {
-    echo "ARCHIVO ENCONTRADO";
-} else {
-    echo "ARCHIVO NO ENCONTRADO";
-}
-
+var_dump($_GET);
 exit;
  
