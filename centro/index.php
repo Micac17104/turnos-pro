2ba764c16178845288a11110d6508a403f798e06
Micaela Cairoli <mcairoli@altec.com.ar>
2026-04-29 01:41:08 -0300

    ss

--- centro/index.php
+++ centro/index.php
@@ -1,14 +1,5 @@
 <?php
-require __DIR__ . '/../config.php';
-
-$slug = $_GET['slug'] ?? null;
-
-if (!$slug) {
-    die("Centro no encontrado.");
-}
-
-// PASAR EL SLUG A LA LANDING COMO VARIABLE
-$slug_centro = $slug;
-
-require __DIR__ . '/centro-landing.php';
+var_dump($_GET);
+var_dump($slug_centro ?? 'NO DEFINIDO');
+exit;
  
