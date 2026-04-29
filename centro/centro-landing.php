436832a331e5f14addf115db8d64dd2d3c5834d6
Micaela Cairoli <mcairoli@altec.com.ar>
2026-04-29 16:36:58 -0300

    prueba mapa dos

--- centro/centro-landing.php
+++ centro/centro-landing.php
@@ -97,7 +97,7 @@ h2 { margin-top:0; font-size:24px; }
             class="map"
             loading="lazy"
             allowfullscreen
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD5-z8qyGQfQVEXLI71BjYZwxrurK2vY3o&q=<?= $direccion_completa ?>">
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDNEW0j2lFJYO6_XZ9qtf4r8dl8M2RC5qI&q=<?= $direccion_completa ?>">
         </iframe>
 
         <p style="font-size:12px; color:#64748b; margin-top:8px;">
