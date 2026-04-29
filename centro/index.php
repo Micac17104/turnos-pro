d632e8ebc3d339a7d3b10a7390831b45c610d9e9
Micaela Cairoli <mcairoli@altec.com.ar>
2026-04-29 01:48:06 -0300

    njn

--- centro/index.php
+++ centro/index.php
@@ -1,5 +1,16 @@
 <?php
-var_dump($_GET);
-var_dump($slug_centro ?? 'NO DEFINIDO');
-exit;
+require __DIR__ . '/../config.php';
+
+// El slug viene desde el .htaccess
+$slug = $_GET['slug'] ?? null;
+
+if (!$slug) {
+    die("Centro no encontrado.");
+}
+
+// Pasamos el slug a la landing
+$slug_centro = $slug;
+
+// Cargar la landing
+require __DIR__ . '/centro-landing.php';
  
